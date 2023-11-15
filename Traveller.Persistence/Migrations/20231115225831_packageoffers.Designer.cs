72809391dbacd9fffe7f1e818f618875f47e8f4f
Enzo D'toste <enzord2001@gmail.com>
2023-11-15 17:59:41 -0500

    migration

--- Traveller.Persistence/Migrations/20231115225831_packageoffers.Designer.cs
+++ Traveller.Persistence/Migrations/20231115225831_packageoffers.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Traveller.Persistence;
@@ -11,9 +12,11 @@ using Traveller.Persistence;
 namespace Traveller.Persistence.Migrations
 {
     [DbContext(typeof(TravellerContext))]
-    partial class TravellerContextModelSnapshot : ModelSnapshot
+    [Migration("20231115225831_packageoffers")]
+    partial class packageoffers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Traveller.Persistence/Migrations/20231115225831_packageoffers.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Traveller.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class packageoffers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PackageOffer_Agencies_AgencyId",
+                table: "PackageOffer");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PackageOffer_Packages_ProductId",
+                table: "PackageOffer");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PackageReservation_PackageOffer_OfferId",
+                table: "PackageReservation");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PackageOffer",
+                table: "PackageOffer");
+
+            migrationBuilder.RenameTable(
+                name: "PackageOffer",
+                newName: "PackageOffers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PackageOffer_ProductId",
+                table: "PackageOffers",
+                newName: "IX_PackageOffers_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PackageOffer_Id",
+                table: "PackageOffers",
+                newName: "IX_PackageOffers_Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PackageOffer_AgencyId",
+                table: "PackageOffers",
+                newName: "IX_PackageOffers_AgencyId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PackageOffers",
+                table: "PackageOffers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PackageOffers_Agencies_AgencyId",
+                table: "PackageOffers",
+                column: "AgencyId",
+                principalTable: "Agencies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PackageOffers_Packages_ProductId",
+                table: "PackageOffers",
+                column: "ProductId",
+                principalTable: "Packages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PackageReservation_PackageOffers_OfferId",
+                table: "PackageReservation",
+                column: "OfferId",
+                principalTable: "PackageOffers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PackageOffers_Agencies_AgencyId",
+                table: "PackageOffers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PackageOffers_Packages_ProductId",
+                table: "PackageOffers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PackageReservation_PackageOffers_OfferId",
+                table: "PackageReservation");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PackageOffers",
+                table: "PackageOffers");
+
+            migrationBuilder.RenameTable(
+                name: "PackageOffers",
+                newName: "PackageOffer");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PackageOffers_ProductId",
+                table: "PackageOffer",
+                newName: "IX_PackageOffer_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PackageOffers_Id",
+                table: "PackageOffer",
+                newName: "IX_PackageOffer_Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PackageOffers_AgencyId",
+                table: "PackageOffer",
+                newName: "IX_PackageOffer_AgencyId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PackageOffer",
+                table: "PackageOffer",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PackageOffer_Agencies_AgencyId",
+                table: "PackageOffer",
+                column: "AgencyId",
+                principalTable: "Agencies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PackageOffer_Packages_ProductId",
+                table: "PackageOffer",
+                column: "ProductId",
+                principalTable: "Packages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PackageReservation_PackageOffer_OfferId",
+                table: "PackageReservation",
+                column: "OfferId",
+                principalTable: "PackageOffer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
