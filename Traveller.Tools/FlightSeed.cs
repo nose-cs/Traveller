96c780aa8378062adee553f171207a6fbf19fe15
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-11-19 11:56:25 -0500

    Add flight seed

---
+++ Traveller.Tools/FlightSeed.cs
@@ -0,0 +1,49 @@
+﻿using Traveller.Domain.Models;
+
+namespace Traveller.Tools;
+
+public partial class Program
+{
+    private static IEnumerable<Flight> Flights = null!; 
+    private const int FlightsCount = 50;
+    
+    private static async Task AddFlightsAsync()
+    {
+        Flights = GenerateFlights();
+        foreach (var flight in Flights)
+        {
+            await _appDbContext.AddAsync(flight);
+        }
+    }
+    
+    private static readonly List<string> Airlines = new()
+    {
+        "Cubana de Aviación",
+        "Aeroflot",
+        "Air France",
+        "British Airways",
+        "Lufthansa",
+        "KLM",
+        "Alitalia",
+        "Iberia",
+        "Aerolíneas Argentinas",
+        "Avianca",
+        "Aeroméxico",
+        "Copa Airlines",
+        "TACA",
+        "TAM",
+        "LAN Airlines",
+        "Aer Caribe"
+    };
+
+    private static IEnumerable<Flight> GenerateFlights()
+    {
+        return Enumerable.Range(1, FlightsCount).Select(_ => new Flight
+        {
+            FlightNumber = Random.Next(100, 1000),
+            Airline = Airlines[Random.Next(0, Airlines.Count)],
+            SourceId = Random.Next(1, PlacesCount),
+            DestinationId = Random.Next(1, PlacesCount)
+        });
+    }
+}
