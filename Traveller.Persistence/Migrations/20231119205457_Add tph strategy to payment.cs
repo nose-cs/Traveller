706435ac144cec197b9afaefff57adab29f33774
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-11-19 15:55:36 -0500

    Add tph strategy to payment

---
+++ Traveller.Persistence/Migrations/20231119205457_Add tph strategy to payment.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Traveller.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addtphstrategytopayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CardNumber",
+                table: "Payment",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Payment",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_Id",
+                table: "Payment",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payment_Id",
+                table: "Payment");
+
+            migrationBuilder.DropColumn(
+                name: "CardNumber",
+                table: "Payment");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Payment");
+        }
+    }
+}
