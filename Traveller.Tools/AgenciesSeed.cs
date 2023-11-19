009452110d7ec9c4ac3f1ae4bdd87ede63bc480f
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-11-19 01:20:52 -0500

    Seed Database

---
+++ Traveller.Tools/AgenciesSeed.cs
@@ -0,0 +1,71 @@
+﻿using Traveller.Domain.Models;
+
+namespace Traveller.Tools;
+
+public partial class Program
+{
+    private static async Task AddAgenciesAsync()
+    {
+        foreach (var agency in GenerateAgencies())
+        {
+            await _appDbContext.AddAsync(agency);
+        }
+    }
+
+    private static IEnumerable<Agency> GenerateAgencies()
+    {
+        return new[]
+        {
+            new Agency
+            {
+                Name = "Sunshine Travel", Fax = "+1-800-123-4567", Email = "sunshine@travel.com",
+                Address = "123 Main Street, New York, NY 10001"
+            },
+            new Agency
+            {
+                Name = "Dream Tours", Fax = "+1-888-987-6543", Email = "dream@tours.com",
+                Address = "456 Park Avenue, Los Angeles, CA 90001"
+            },
+            new Agency
+            {
+                Name = "Paradise Holidays", Fax = "+1-877-765-4321", Email = "paradise@holidays.com",
+                Address = "789 Broadway, Chicago, IL 60601"
+            },
+            new Agency
+            {
+                Name = "Explorer Adventures", Fax = "+1-866-543-2109", Email = "explorer@adventures.com",
+                Address = "101 Market Street, San Francisco, CA 94101"
+            },
+            new Agency
+            {
+                Name = "Star Travel", Fax = "+1-855-321-0987", Email = "star@travel.com",
+                Address = "202 Main Street, Boston, MA 02101"
+            },
+            new Agency
+            {
+                Name = "Wonder Tours", Fax = "+1-844-210-9876", Email = "wonder@tours.com",
+                Address = "303 Park Avenue, Miami, FL 33101"
+            },
+            new Agency
+            {
+                Name = "Bliss Holidays", Fax = "+1-833-098-7654", Email = "bliss@holidays.com",
+                Address = "404 Broadway, Seattle, WA 98101"
+            },
+            new Agency
+            {
+                Name = "Adventure Zone", Fax = "+1-822-765-4320", Email = "adventure@zone.com",
+                Address = "505 Market Street, Denver, CO 80201"
+            },
+            new Agency
+            {
+                Name = "Sky Travel", Fax = "+1-811-432-1098", Email = "sky@travel.com",
+                Address = "606 Main Street, Houston, TX 77001"
+            },
+            new Agency
+            {
+                Name = "Magic Tours", Fax = "+1-800-109-8765", Email = "magic@tours.com",
+                Address = "707 Park Avenue, Atlanta, GA 30301"
+            }
+        };
+    }
+}
---
+++ Traveller.Tools/HotelsSeed.cs
@@ -0,0 +1,171 @@
+﻿using Traveller.Domain.Models;
+
+namespace Traveller.Tools;
+
+public partial class Program
+{
+    private static async Task AddHotelsAsync()
+    {
+        foreach (var hotel in GenerateHotels())
+        {
+            await _appDbContext.AddAsync(hotel);
+        }
+    }
+
+    private static IEnumerable<Hotel> GenerateHotels()
+    {
+        return new[]
+        {
+            new Hotel
+            {
+                Name = "The Grand Hotel",
+                Address = "123 Main Street, New York, NY",
+                Category = Category.FourStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz Carlton",
+                Address = "456 Broadway, Los Angeles, CA",
+                Category = Category.ThreeStars
+            },
+            new Hotel
+            {
+                Name = "The Plaza Hotel",
+                Address = "789 Fifth Avenue, New York, NY",
+                Category = Category.FiveStars
+            },
+            new Hotel
+            {
+                Name = "The Waldorf Astoria",
+                Address = "321 Park Avenue, New York, NY",
+                Category = Category.ThreeStars
+            },
+            new Hotel
+            {
+                Name = "The Four Seasons",
+                Address = "654 Broadway, New York, NY",
+                Category = Category.TwoStars
+            },
+            new Hotel
+            {
+                Name = "The Beverly Wilshire",
+                Address = "987 Wilshire Boulevard, Los Angeles, CA",
+                Category = Category.FourStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Delaire",
+                Address = "111 Del Mar Drive, Delaire, DE",
+                Category = Category.FiveStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Cancun",
+                Address = "222 Cancun Drive, Cancun, MX",
+                Category = Category.FourStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Bangkok",
+                Address = "333 Silom Road, Bangkok, TH",
+                Category = Category.FiveStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, St. Thomas",
+                Address = "444 St. Thomas Drive, St. Thomas, VI",
+                Category = Category.OneStar
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Paris",
+                Address = "555 Champs-Elysées, Paris, FR",
+                Category = Category.FiveStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Rome",
+                Address = "666 Via Veneto, Rome, IT",
+                Category = Category.FourStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Geneva",
+                Address = "777 Rue du Rhône, Geneva, CH",
+                Category = Category.FiveStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, San Francisco",
+                Address = "888 Union Square, San Francisco, CA",
+                Category = Category.FiveStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Boston",
+                Address = "999 Commonwealth Avenue, Boston, MA",
+                Category = Category.ThreeStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, San Diego",
+                Address = "1111 Horton Plaza, San Diego, CA",
+                Category = Category.FourStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Chicago",
+                Address = "2222 Michigan Avenue, Chicago, IL",
+                Category = Category.ThreeStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Miami",
+                Address = "3333 Collins Avenue, Miami, FL",
+                Category = Category.OneStar
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Honolulu",
+                Address = "4444 Kalakaua Avenue, Honolulu, HI",
+                Category = Category.TwoStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Orlando",
+                Address = "5555 International Drive, Orlando, FL",
+                Category = Category.ThreeStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Las Vegas",
+                Address = "6666 Las Vegas Boulevard South, Las Vegas, NV",
+                Category = Category.FourStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, San Antonio",
+                Address = "7777 Commerce Street, San Antonio, TX",
+                Category = Category.FiveStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Dallas",
+                Address = "8888 Market Center Drive, Dallas, TX",
+                Category = Category.TwoStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Atlanta",
+                Address = "9999 Peachtree Street, Atlanta, GA",
+                Category = Category.FourStars
+            },
+            new Hotel
+            {
+                Name = "The Ritz-Carlton, Seattle",
+                Address = "11111 University Street, Seattle, WA",
+                Category = Category.ThreeStars
+            }
+        };
+    }
+}
