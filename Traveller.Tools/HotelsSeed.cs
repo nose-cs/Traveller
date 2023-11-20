f21793334924a60a6e0a82ba2458feacfed13836
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-11-19 20:16:53 -0500

    Refactor

--- Traveller.Tools/HotelsSeed.cs
+++ Traveller.Tools/HotelsSeed.cs
@@ -16,160 +16,20 @@ public partial class Program
         }
     }
 
+    private static List<string> HotelNames = new()
+    {
+        "The Grand Hotel", "The Ritz Carlton", "The Plaza Hotel", "The Waldorf Astoria", "The Four Seasons",
+        "The Beverly Wilshire", "The Ritz-Carlton", "The Peninsula", "The Savoy", "The Mandarin Oriental",
+        "The Dorchester", "The Langham", "The Connaught", "The Berkeley", "The Lanesborough", "The Shangri-La",
+    };
+
     private static IEnumerable<Hotel> GenerateHotels()
     {
-        return new[]
+        return Enumerable.Range(1, HotelCount).Select(i => new Hotel
         {
-            new Hotel
-            {
-                Name = "The Grand Hotel",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FourStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz Carlton",
-                AddressId = Random.Next(1, 200),
-                Category = Category.ThreeStars
-            },
-            new Hotel
-            {
-                Name = "The Plaza Hotel",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FiveStars
-            },
-            new Hotel
-            {
-                Name = "The Waldorf Astoria",
-                AddressId = Random.Next(1, 200),
-                Category = Category.ThreeStars
-            },
-            new Hotel
-            {
-                Name = "The Four Seasons",
-                AddressId = Random.Next(1, 200),
-                Category = Category.TwoStars
-            },
-            new Hotel
-            {
-                Name = "The Beverly Wilshire",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FourStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Delaire",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FiveStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Cancun",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FourStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Bangkok",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FiveStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, St. Thomas",
-                AddressId = Random.Next(1, 200),
-                Category = Category.OneStar
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Paris",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FiveStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Rome",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FourStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Geneva",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FiveStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, San Francisco",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FiveStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Boston",
-                AddressId = Random.Next(1, 200),
-                Category = Category.ThreeStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, San Diego",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FourStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Chicago",
-                AddressId = Random.Next(1, 200),
-                Category = Category.ThreeStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Miami",
-                AddressId = Random.Next(1, 200),
-                Category = Category.OneStar
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Honolulu",
-                AddressId = Random.Next(1, 200),
-                Category = Category.TwoStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Orlando",
-                AddressId = Random.Next(1, 200),
-                Category = Category.ThreeStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Las Vegas",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FourStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, San Antonio",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FiveStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Dallas",
-                AddressId = Random.Next(1, 200),
-                Category = Category.TwoStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Atlanta",
-                AddressId = Random.Next(1, 200),
-                Category = Category.FourStars
-            },
-            new Hotel
-            {
-                Name = "The Ritz-Carlton, Seattle",
-                AddressId = Random.Next(1, 200),
-                Category = Category.ThreeStars
-            }
-        };
+            Name = HotelNames[i % HotelNames.Count],
+            AddressId = Random.Next(1, PlacesCount),
+            Category = (Category)Random.Next(1, 5)
+        });
     }
 }
