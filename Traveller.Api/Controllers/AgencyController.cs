b5c8744bef6dde8dd1f6f064e7cbc2e663896c07
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-11-25 17:30:30 -0500

    Add agencyUserController

--- Traveller.Api/Controllers/AgencyController.cs
+++ Traveller.Api/Controllers/AgencyController.cs
@@ -1,21 +1,19 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Traveller.Domain;
-using Traveller.Domain.Models;
 using Traveller.Dtos;
-using Traveller.Exceptions;
 using Traveller.Services;
 
 namespace Traveller.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AgencyController : ControllerBase
+[Authorize(Roles = ("TravellerAdmin"))]
+public partial class AgencyController : ControllerBase
 {
     private readonly Repositories _repositories;
 
     private readonly ILogger<HotelController> _logger;
-    private readonly LoginService _loginService;
 
     public AgencyController(Repositories repositories, ILogger<HotelController> logger, LoginService loginService)
     {
@@ -81,7 +79,7 @@ public class AgencyController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<FlightDto>> GetAll() => Ok(_repositories.Flights.Find().Select(FlightDto.Map));
+    public ActionResult<IEnumerable<AgencyDto>> GetAll() => Ok(_repositories.Agencies.Find().Select(AgencyDto.Map));
 
     [HttpGet("{id:int}")]
     public async Task<ActionResult> Get([FromRoute] int id)
@@ -101,79 +99,5 @@ public class AgencyController : ControllerBase
             _logger.LogError(e.Message);
             return StatusCode(StatusCodes.Status500InternalServerError);
         }
-    }
-
-    [HttpPost("{id:int}/register")]
-    public async Task<IActionResult> RegisterUser(UserDto userDto, int id)
-    {
-        var token = Request.Headers.Authorization[0]![7..];
-        var jwt = new JwtSecurityToken(token);
-        var agencyId = int.Parse(jwt.Claims.First(c => c.Type == "agencyId").Value);
-        
-        if (agencyId != id || userDto.Role is not (Role.AgencyAdmin or Role.Agent or Role.MarketingEmployee)) 
-            return Unauthorized("You don't have permission for this action");
-
-        try
-        { 
-            await _loginService.CreateAccount(userDto, agencyId);
-            return Ok();
-        }
-        catch (BadRequestException e)
-        {
-            return BadRequest(e.Message);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e.Message);
-            return StatusCode(StatusCodes.Status500InternalServerError);
-        }
-    }
-    
-    [HttpPost("{idAgency:int}/employees/{idUser:int}")]
-    public async Task<IActionResult> UpdateUser(UserDto userDto, int idUser, int idAgency)
-    {
-        try
-        {
-            var token = Request.Headers.Authorization[0]![7..];
-            var jwt = new JwtSecurityToken(token);
-            var agencyId = int.Parse(jwt.Claims.First(c => c.Type == "agencyId").Value);
-        
-            if (agencyId != idAgency || userDto.Role is not (Role.AgencyAdmin or Role.Agent or Role.MarketingEmployee)) 
-                return Unauthorized("You don't have permission for this action");
-            
-            var dbUser = await _repositories.Users.FindById(idUser);
-            if (dbUser is AgencyUser agencyUser && agencyUser.AgencyId != idAgency || dbUser is null)
-            {
-                return NotFound($"User with id {idUser} doesn't exist in agency {idAgency}");
-            }
-            
-            dbUser.Email = userDto.Email;
-            dbUser.Password = userDto.Password;
-            dbUser.Role = userDto.Role;
-            dbUser.Name = userDto.Name;
-            
-            await _repositories.Hotels.SaveChangesAsync();
-
-            return Ok();
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e.Message);
-            return StatusCode(StatusCodes.Status500InternalServerError);
-        }
-    }
-    
-    [HttpGet("{id:int}/employees")]
-    public IActionResult GetUsers(int id)
-    {
-        //TODO
-        return Ok();
-    }
-    
-    [HttpGet("{idAgency:int}/employees/{idUser:int}/reservations")]
-    public IActionResult GetUserReservations([FromRoute] int idAgency, [FromRoute] int idUser)
-    {
-        //TODO
-        return Ok();
     }
 }
