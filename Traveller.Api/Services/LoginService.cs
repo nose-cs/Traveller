1b13b519c8eef139780041a21657ee07b2ede22a
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-12-01 00:33:36 -0500

    Fix update

--- Traveller.Api/Services/LoginService.cs
+++ Traveller.Api/Services/LoginService.cs
@@ -1,4 +1,5 @@
 ﻿using Traveller.Domain.Interfaces.Repositories;
+using Traveller.Domain.Models;
 using Traveller.Dtos;
 using Traveller.Exceptions;
 
@@ -50,5 +51,33 @@ public class LoginService
 
         var token = _jwtProvider.Generate(user);
         return token;
+    }
+
+    public async Task UpdateAgencyUserAccount(int idUser, int agencyId, UserDto userDto)
+    {
+        var dbUser = await _repository.FindById(idUser);
+        if (dbUser is AgencyUser agencyUser && agencyUser.AgencyId != agencyId || dbUser is null)
+        {
+            throw new NotFoundException($"User with id {idUser} doesn't exist in agency {agencyId}");
+        }
+            
+        var user = await _repository.FindByEmail(userDto.Email);
+        
+        if (user is not null)
+        {
+            throw new BadRequestException($"Email {userDto.Email} already exists");
+        }
+        
+        if (userDto.Password is null)
+        {
+            throw new BadRequestException("Password can't be null");
+        }
+            
+        dbUser.Email = userDto.Email;
+        dbUser.Password = _passwordService.EncryptPassword(userDto.Password);
+        dbUser.Role = userDto.Role;
+        dbUser.Name = userDto.Name;
+            
+        await _repository.SaveChangesAsync();
     }
 }
