d42c9b5281e7637ead66f61fa1e2b61b00d623d2
Enzo D'toste <enzord2001@gmail.com>
2023-12-03 18:26:12 -0500

    pagination for reservation, show valid offers

--- Traveller.Api/Controllers/FlightReservationController.cs
+++ Traveller.Api/Controllers/FlightReservationController.cs
@@ -148,19 +148,43 @@ public class FlightReservationController : ControllerBase
 
     [HttpGet]
     [Authorize(Roles = ("Agent, Tourist"))]
-    public ActionResult<IEnumerable<ReservationDto>> GetAll()
+    public ActionResult<IEnumerable<ReservationDto>> GetAll([FromQuery] PaginationDto filter)
     {
         var token = Request.Headers.Authorization[0]![7..];
         var jwt = new JwtSecurityToken(token);
         var role = jwt.Claims.First(c => c.Type == "role").Value;
         var userId = int.Parse(jwt.Claims.First(c => c.Type == "id").Value);
 
-        var dtos = _repositories.FlightReservations.Find()
-            .Select(ReservationDto.Map<Flight, FlightReservation, FlightOffer>);
+        var items = _repositories.FlightReservations.FindWithInclude(f => f.Offer);
 
         if (role == "Tourist")
-            dtos = dtos.Where(rsv => rsv.TouristId == userId);
-        return Ok(dtos);
+            items = items.Where(rsv => rsv.TouristId == userId);
+        else {
+            var agencyId = int.Parse(jwt.Claims.First(c => c.Type == "agencyId").Value);
+
+            items = items.Where(rsv => rsv.Offer.AgencyId == agencyId);
+        }
+
+        if (filter.OrderBy != null)
+        {
+            switch (filter.OrderBy)
+            {
+                case ("Price"):
+                    items = items.OrderBy(item => item.Price); break;
+                case ("DepartureDay"):
+                    items = items.OrderBy(item => item.DepartureDate); break;
+                default:
+                    items = items.OrderBy(item => item.Id); break;
+            }
+        }
+
+        if (filter.Descending.HasValue && filter.Descending.Value)
+            items = items.Reverse();
+
+        var pageItems = (filter.PageIndex == null || filter.PageSize == null ? items : items.Take(new Range((filter.PageIndex.Value - 1) * filter.PageSize.Value, (filter.PageIndex.Value - 1) * filter.PageSize.Value + filter.PageSize.Value)))
+                            .Select(ReservationDto.Map<Flight, FlightReservation, FlightOffer>);
+
+        return Ok(new PaginationResponse<ReservationDto>() { TotalCollectionSize = items.Count(), Items = pageItems });
     }
 
     [HttpGet("{id:int}")]
@@ -175,10 +199,23 @@ public class FlightReservationController : ControllerBase
         try
         {
             var flightReservation = await _repositories.FlightReservations.FindById(id);
+            
             if (flightReservation is null)
                 return NotFound($"Flight reservation with id {id} doesn't exist");
+            
             if ((role == "Tourist") && (userId != flightReservation.TouristId))
                 return BadRequest($"Tourists can only see their own reservations");
+            
+            if(role == "Agent")
+            {
+                var agencyId = int.Parse(jwt.Claims.First(c => c.Type == "agencyId").Value);
+
+                var offer = await _repositories.FlightOffers.FindById(flightReservation.OfferId);
+
+                if(agencyId != offer!.AgencyId)
+                    return BadRequest("Agents can only see their own agency reservations");
+            }
+
             return Ok(ReservationDto.Map<Flight, FlightReservation, FlightOffer>(flightReservation));
         }
         catch (Exception e)
