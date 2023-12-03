dc0ed06b2f80d97f6096f8ca74bba8ffb023fde1
Enzo D'toste <enzord2001@gmail.com>
2023-12-03 00:48:40 -0500

    edit package

---
+++ Traveller.Api/Controllers/FacilityController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using Traveller.Domain;
+using Traveller.Domain.Models;
+using Traveller.Dtos;
+using Traveller.Services;
+
+namespace Traveller.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FacilityController : ControllerBase
+{
+    private readonly Repositories _repository;
+    private readonly ILogger<TourOfferController> _logger;
+
+    public FacilityController(ILogger<TourOfferController> logger, Repositories repository, ExporterService exporterService)
+    {
+        _logger = logger;
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public IActionResult GetFacilities([FromQuery] FacilityFilterDto filter)
+    {
+        var fs = _repository.Facilities.Find().Where(f =>
+            (filter.Id == null || f.Id == filter.Id)
+            && (filter.Name == null || f.Name.ToLower().Contains(filter.Name.ToLower()))
+            && (filter.Description == null || f.Description.ToLower().Contains(filter.Description)));
+
+        if (filter.OrderBy != null)
+        {
+            switch (filter.OrderBy)
+            {
+                case ("Name"):
+                    fs = fs.OrderBy(offer => offer.Name); break;
+                default:
+                    fs = fs.OrderBy(offer => offer.Id); break;
+            }
+        }
+
+        if (filter.Descending.HasValue && filter.Descending.Value)
+            fs = fs.Reverse();
+
+        var pagefs = (filter.PageIndex == null || filter.PageSize == null ? fs : fs.Take(new Range((filter.PageIndex.Value - 1) * filter.PageSize.Value, (filter.PageIndex.Value - 1) * filter.PageSize.Value + filter.PageSize.Value)))
+                               .ToArray().Select(f => new FacilityDto(f.Id, f.Name, f.Description));
+                              
+
+        return Ok(new PaginationResponse<FacilityDto>() { TotalCollectionSize = fs.Count(), Items = pagefs });
+    }
+}
---
+++ Traveller.Api/Dtos/PackageFacilityDto.cs
@@ -0,0 +1,23 @@
+﻿using Traveller.Domain.Models;
+
+namespace Traveller.Dtos
+{
+    public record FacilityDto(int Id, string Name, string Description);
+    public record FacilityFilterDto(int? Id, string? Name, string? Description, int? PageIndex, int? PageSize, string? OrderBy, bool? Descending);
+    public class PackageFacilityDto
+    {
+        public double Price { get; set; }
+        public int PackageId { get; set; }
+        public FacilityDto Facility { get; set; }
+
+        public static PackageFacilityDto Map(PackageFacility packageFacility)
+        {
+            return new PackageFacilityDto() { 
+                Price = packageFacility.Price, 
+                PackageId = packageFacility.PackageId, 
+                Facility = new FacilityDto(packageFacility.Facility.Id, packageFacility.Facility.Name, packageFacility.Facility.Description)
+            };
+        }
+    }
+}
+
